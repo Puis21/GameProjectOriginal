db47898475e872cb3d47d741f9a6f6d90174e69e
Puis21 <47056535+Puis21@users.noreply.github.com>
2020-02-27 13:28:59 +0100

    Commit 1
    Added basic movement and camera control to player and an object that can be controlled.
    Also started the shoot spirit feature.

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
 
-    int speed;
+    // public CharacterController controller;
+    Rigidbody playerRb;
+
+    public float speed = 12f;
+    public float jumpForce = 4f;
+
+    public LayerMask groundLayers;
+
+    public CapsuleCollider col;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        */
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z),
+            col.radius * .9f, groundLayers);
     }
 }
---
+++ Assets/PlayerLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 5f;
+    //public float smoothing = 2.0f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    //GameObject character;
+   // Vector2 smoothV;
+   // Vector2 mouseLook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //character = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float mouseX = Input.GetAxis("MouseX") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("MouseY") * mouseSensitivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+        /*
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(mouseSensitivity * smoothing, mouseSensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        playerBody.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, playerBody.transform.up);
+        */
+
+    }
+}
