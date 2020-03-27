30e5a503c6ced95b9af5d07101fece64dfd1690b
Puis21 <47056535+Puis21@users.noreply.github.com>
2020-03-27 19:13:42 +0100

    Main Mechanic #3
    Added extra code so the script for the player moving is disable while controlling an object.

--- Assets/ObjectDecetion.cs
+++ Assets/ObjectDecetion.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class ObjectDecetion : MonoBehaviour
 {
+
+    GameObject playerMove;
+
+    private void Start()
+    {
+        playerMove = GameObject.FindGameObjectWithTag("Player");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Spirit")
         {
             transform.GetChild(0).gameObject.SetActive(true);
+            playerMove.GetComponent<MovementScript>().enabled = false;
             Debug.Log("MeHIt");
         }
     }
